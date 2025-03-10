4f98088dab47f64cbfa7ae281ea2f7adb2a35101
Sovch <valentinafediakova@gmail.com>
2025-03-10 19:46:19 +0400

    add button to open ActionBar

--- src/components/ActionBar/ActionBar.tsx
+++ src/components/ActionBar/ActionBar.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { DrawingManager } from '@/utils/DrawingManager';
 import './ActionBar.scss';
 
@@ -6,22 +8,29 @@ interface ActionBarProps {
 }
 
 export const ActionBar: React.FC<ActionBarProps> = ({ drawingManagerRef }) => {
+  const [actionBarOpen, setActionBarOpen] = useState(false);
+
   const handleClear = () => {
     drawingManagerRef.current?.clearCanvas();
   };
 
   return (
-    <div className="action-bar">
-      {/* <button className="action-bar__item">
-        <div className="action-bar__undo"></div>
+    <>
+      <button className={`action-bar__button-open`} onClick={() => setActionBarOpen(!actionBarOpen)}>
+        <div className={actionBarOpen ? `action-bar__button-open-img action-bar__button-open-img_opacity` : `action-bar__button-open-img`}></div>
       </button>
-      <button className="action-bar__item">
-        <div className="action-bar__redo"></div>
-      </button> */}
-      <button className="action-bar__item" onClick={handleClear}>
-        <div className="action-bar__clear-img action-bar__clear-img_trash"></div>
-        <div className="action-bar__clear-title">Очистить лист</div>
-      </button>
-    </div>
+      <div className={actionBarOpen ? 'action-bar action-bar_open' : 'action-bar'}>
+        {/* <button className="action-bar__item">
+          <div className="action-bar__undo"></div>
+        </button>
+        <button className="action-bar__item">
+          <div className="action-bar__redo"></div>
+        </button> */}
+        <button className="action-bar__item" onClick={handleClear}>
+          <div className="action-bar__clear-img action-bar__clear-img_trash"></div>
+          <div className="action-bar__clear-title">Очистить лист</div>
+        </button>
+      </div>
+    </>
   );
 }
