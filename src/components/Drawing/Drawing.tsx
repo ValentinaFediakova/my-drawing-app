8b760f4a50cf6e07bf48ad0034ab41778bc504b6
Sovch <valentinafediakova@gmail.com>
2025-03-10 20:17:38 +0400

    fix lint error

--- src/components/Drawing/Drawing.tsx
+++ src/components/Drawing/Drawing.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { DrawingManager } from "@/utils/DrawingManager";
