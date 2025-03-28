6c9b46c23d606a741914132bec1b3fd58d8ec478
Sovch <valentinafediakova@gmail.com>
2025-03-28 22:44:38 +0400

    check connection to ws

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -14,6 +14,13 @@ export const Canvas: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const drawingManagerRef = useRef<DrawingManager | null>(null);
 
+  const ws = new WebSocket("wss://my-drawing-app-production.up.railway.app");
+
+  ws.onopen = () => console.log("✅ Connected to WebSocket!");
+  ws.onerror = (err) => console.error("❌ WebSocket Error:", err);
+  ws.onmessage = (msg) => console.log("💬 Received:", msg.data);
+  ws.onclose = () => console.log("🔴 Disconnected from WebSocket");
+
 
   return (
     <div className={`canvas-wrapper`} style={{ backgroundColor: DEFAULT_BG_COLOR }}>
