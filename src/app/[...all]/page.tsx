8941519909fd0ab8b94d4980ffa6de1b1d3a4fc5
Sovch <valentinafediakova@gmail.com>
2025-04-10 19:10:14 +0400

    delete console.log

--- src/app/[...all]/page.tsx
+++ src/app/[...all]/page.tsx
@@ -7,8 +7,6 @@ import { Canvas } from "@/components/Canvas/Canvas";
 export default function AllRoutes() {
   const pathname = usePathname();
 
-  console.log("🚦 Current pathname:", pathname);
-
   return (
     <ReduxProvider>
       {pathname === "/canvas" && <Canvas />}
