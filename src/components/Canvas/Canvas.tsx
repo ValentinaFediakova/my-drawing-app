975f2c0fc66968585b0856a404b61be75c06d74a
Sovch <valentinafediakova@gmail.com>
2025-04-17 18:48:49 +0400

    protect /canvas route with token check

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,18 +1,33 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Drawing } from "../Drawing/Drawing";
 import { ActionBar } from "../ActionBar/ActionBar";
 import { Toolbar } from "../Toolbar/Toolbar";
 import { Settings } from "../Settings/Settings";
 import { DEFAULT_BG_COLOR } from "@/constants";
 import { DrawingManager } from "@/utils/DrawingManager";
+import { useRouter } from "next/navigation";
 
 import "./Canvas.scss";
 
 export const CanvasPage: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const drawingManagerRef = useRef<DrawingManager | null>(null);
+  const [isChecking, setIsChecking] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.replace("/auth");
+    } else {
+      setIsChecking(false);
+    }
+  }, []);
+
+  if (isChecking) return null;
 
   return (
     <div className={`canvas-wrapper`} style={{ backgroundColor: DEFAULT_BG_COLOR }}>
